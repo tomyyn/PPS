412d32f31eb7d7312ce7cdd33f152259eaade077
Tomas Naiouf <tomasnaiouf@gmail.com>
2021-09-26 20:24:38 -0300

    Tarea 0 finalizada

--- Tareas/Tarea 0/GraficarBB.m
+++ Tareas/Tarea 0/GraficarBB.m
@@ -1,4 +1,4 @@
-#Función encargada de graficar la DEP en banda base.
+#Script encargado de graficar la DEP en banda base.
 #Parámetros:
 # A: amplitud del pulso
 # Br: tasa de bits
@@ -6,7 +6,7 @@
 function GraficarBB(A,Br)
   #Cálculo del tiempo de bit.
   T = 1/Br;
-  #Creación del valor de frecuencias con valores límite que permitan ver
+  #Creación del vector de frecuencias con valores límite que permitan ver
   #los dos primeros lóbulos del sinc.
   window = 4*Br;
   f = -window: 0.01:window;
--- Tareas/Tarea 0/GraficarPB.m
+++ Tareas/Tarea 0/GraficarPB.m
@@ -1,4 +1,4 @@
-#Función encargada de graficar la DEP en pasa banda.
+#Script encargado de graficar la DEP en pasa banda.
 #Parámetros:
 # A: amplitud del pulso
 # Br: tasa de bits
@@ -7,15 +7,15 @@
 function GraficarPB(A,Br,fp)
   #Cálculo del tiempo de bit
   T = 1/Br;
-  #Creación del valor de frecuencias con valores límite que permitan ver
-  #los dos primeros lóbulos del sinc.
+  #Creación del vector de frecuencias con valores límite que permitan ver
+  #los dos primeros lóbulos de los sincs.
   window = 4*Br + fp;
   f = -window: 0.01:window;
   #Obtención de Syy.
-  Sxx = DEPBB(f-fp,T,A) + DEPBB(f+fp,T,A);
+  Sxx = (DEPBB(f-fp,T,A) + DEPBB(f+fp,T,A))/2;
   #Gráfico Syy.
   xlim([-window, window]);
   plot(f,Sxx);
   xlabel("f")
-  ylabel("Sxx(f)")
+  ylabel("Syy(f)")
 endfunction
