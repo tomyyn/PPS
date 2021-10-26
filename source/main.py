a7cf5b22244892defde4d220986b760d44f60f57
tomyyn <tomasnaiouf@gmail.com>
2021-10-26 17:37:01 +0200

    Generación de señales sin doppler/Pérdida

--- source/main.py
+++ source/main.py
@@ -1,6 +1,17 @@
 """from UI import *"""
 from signalGen import *
-print(num_a_bin(randrange()))
+
+msg = generar_msg(3, 1)
+
+msg = (msg_a_pulso(msg))
+msg = pulso_a_senal(msg, 10000)
+
+print(msg.shape[0])
+
+dep = np.asarray(sig.welch(msg, fs, return_onesided=False))
+plt.plot(dep[0], dep[1])
+plt.show()
+
 
 """
 cod = NE
--- source/signalGen.py
+++ source/signalGen.py
@@ -3,25 +3,89 @@ import scipy.signal as sig
 import scipy.sparse as sparse
 import matplotlib.pyplot as plt
 from random import randrange
+from math import cos
+from math import sin
+from math import ceil
 
 fs = 44100
+bps = 400
+mpb = fs/bps
+
+A = 1
+alfa = 1.1
+a = cos(alfa)
+b = sin(alfa)
+
+tpor =0.16
+npor = ceil(tpor * fs)
+
+cantID = 1048576
+
+
+escalera = np.array([0, 0, 0, 0,
+                     0, 0, 0, 1,
+                     0, 0, 1, 0,
+                     0, 0, 1, 1,
+                     0, 1, 0, 0,
+                     0, 1, 0, 1,
+                     0, 1, 1, 0,
+                     0, 1, 1, 1], dtype=int)
+
+pulsoManchester = np.concatenate((np.ones(int(mpb/2), dtype=int), -(np.ones(int(mpb/2), dtype=int))))
 
 hdr = np.concatenate((np.array(np.ones(15, dtype=int)),
                       np.array([0, 0, 0, 1, 0, 1, 1, 1], dtype=int),
                       np.array([1], dtype=int)
 ))
 
-def num_a_bin(num):
+
+
+def num_a_bin(num, size = None):
     b = bin(num)[2:]
     vec = np.array([], dtype=int)
     for i in b:
         vec = np.append(vec, np.array([int(i)], dtype=int))
+
+    if(size != None):
+        vec = np.append(np.zeros(size - vec.shape[0], dtype=int), vec)
+
     return vec
 
-def generar_msg():
-    msg = hdr
-    id
-    return msg
+def generar_msg(pID = randrange(cantID), N = randrange(1,9)):
+
+
+    msg = np.array([])
+    for i in range(N):
+        msg = np.append(msg, escalera)
+
+    N = num_a_bin(N,4)
+    pID = num_a_bin(pID, 20)
+
+    return np.concatenate((hdr, N, pID, msg))
+
+def msg_a_pulso(msg):
+    msg = (msg * 2)-1
+
+    return sparse.kron(msg, pulsoManchester).toarray()[0]
+
+def doppler():
+    print("TBD")
+    return 0
+
+def pel():
+    print("TBD")
+    return 0
+
+def pulso_a_senal(msg, fp, dop = 0, deg = 1):
+    t = np.arange(0, tpor + msg.shape[0]/fs, 1/fs)
+    portadora = A * np.cos(2 * np.pi * (fp+dop)*t) / deg + 0j
+    portadora[npor:] = portadora[npor:]*msg*(a+1j*b)
+
+    print(portadora[npor:])
+
+    return portadora
+
+
 """
 fig = plt.figure()
 f = np.arange(0, fs-1/fs, 1/fs)
